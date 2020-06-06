9fefcf439bab2e4b52628381a71a22ff3946441d
Doinkyyy <hayagura2698@gmail.com>
2020-06-06 23:46:18 +0800

    Updates

---
+++ precache-manifest.a627e658402bc2567b233368b2278faa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a93ce5964486de1c2ed7279b8173633",
+    "url": "/yommyy.github.io/react-deploy/index.html"
+  },
+  {
+    "revision": "0df66ff0a3715f761e7d",
+    "url": "/yommyy.github.io/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fd4f50a7992c56f254ec",
+    "url": "/yommyy.github.io/react-deploy/static/js/2.ae637d20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yommyy.github.io/react-deploy/static/js/2.ae637d20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0df66ff0a3715f761e7d",
+    "url": "/yommyy.github.io/react-deploy/static/js/main.f8fd8f34.chunk.js"
+  },
+  {
+    "revision": "962019dba5454750930e",
+    "url": "/yommyy.github.io/react-deploy/static/js/runtime-main.bf840ca9.js"
+  }
+]);
